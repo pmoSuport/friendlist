3ae198eba504ad2dddf2a9af96bcd5599682e90c
sunsingh162 <sunsingh162@gmail.com>
2021-09-11 01:41:48 +0530

    friends-list

---
+++ src/mockData/friendList.js
@@ -0,0 +1,39 @@
+const friendList = [
+    {
+      id: '1',
+      name: 'MS Dhoni',
+      favorite: false
+    },
+    {
+      id: '2',
+      name: 'Virat Kohli',
+      favorite: false
+    },
+    {
+      id: '3',
+      name: 'Rohit Sharma',
+      favorite: false
+    },
+    {
+      id: '4',
+      name: 'Surya Kumar',
+      favorite: false
+    },
+    {
+      id: '5',
+      name: 'Rishabh Pant',
+      favorite: false
+    },
+    {
+      id: '6',
+      name: 'Hardik Pandya',
+      favorite: false
+    },
+    {
+      id: '7',
+      name: 'Jasprit Bumrah',
+      favorite: false 
+    },
+  ];
+  
+  export default friendList;
